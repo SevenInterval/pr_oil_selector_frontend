4e527d7cceee0437b7678d5f79abca3db80710ea
duyguudoner <duyguudoner@gmail.com>
2024-03-26 22:02:17 +0300

    Footer add

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,221 @@
+import Footer_bg from '../images/footer-bg-repeat-x.png'
+import F_icon from '../images/F_icon.svg.png'
+
+
 const Footer = () => {
     return (
-        <div>
+        <footer>
+            <div className="top" style={{ backgroundImage: `url(${Footer_bg})` }}>
+                <div className="container">
+                    <h4 class="global-h">İrtibat</h4>
+                    <div class="row">
+                        <div class="span">
+                            <h4>Bulgaristan</h4>
+                            <div class="body">
+                                <p>Prista Oil Holding HQ
+                                <br></br>
+                                1407 Sofia
+                                <br></br>
+                                20 Zlaten Rog St.
+                                <br></br>
+                                tel: + 359 2 9620110
+                                <br></br>
+                                fax: +359 2 8689476
+                                <br></br>
+                                    <a href="mailto:office@prista-oil.bg" > office@prista-oil.bg</a>
+                                    <br></br>
+                                    <a href="http://www.prista-oil.com/" > http://www.prista-oil.com/</a>
+                                    <br></br>
+                                </p>
+                                <p>Prista Oil Blending Plant
+                                <br></br>
+                                    <br></br>
+                                7012 Ruse
+                                <br></br>
+                                46 Treti Mart Blvd.
+                            </p>
+                                <p>
+                                    <a href="https://www.facebook.com/PristaOil/" target="_blank" style={{ backgroundImage: `url(${F_icon})` }}>
+                                    </a>
+                                </p>
+                            </div>
+                        </div>
+                        <div class="span">
+                            <h4>Romanya</h4>
+                            <div class="body">
+                                <p>Prista Oil Romania S.A.
+                            <br></br>
+                            14459 Bucharest
+                            <br></br>
+                            169 Calea Floreasca
+                            <br></br>
+                            tel: +40 021 4115517, + 40 0372734701
+                            <br></br>
+                            fax: + 40 021 4115515/16/18
+                            <br></br>
+                                    <a href="mailto:office@prista-oil.ro"> office@prista-oil.ro</a>
+                                    <br></br>
+                                    <a href="http://www.prista-oil.ro/" > www.prista-oil.ro</a>
+                                    <br></br>
+                                </p>
+                                <p>
+                                    <a href="https://www.facebook.com/PristaOilRomania/" target="_blank" >
+                                    </a>
+                                </p>
+                            </div>
+                            <h4>Rusya</h4>
+                            <div >
+                                <p>
+                                    LLC «Inventum»
+                            <br></br>
+                            Prospect Mira 101, building 1,
+                            <br></br>
+                            E / POM / K / OF 4 / l /14/61.
+                            <br></br>
+                            Moscow 129085
+                            <br></br>
+                            Russia
+                            <br></br>
+                                </p>
+                                <a href="mailto:info@pristagroup.com" target="_blank" >info@pristagroup.com
+                            </a>
+                            </div>
 
-        </div>
+                        </div>
+                        <div class="span">
+                            <h4>Macaristan</h4>
+                            <div class="body">
+                                <p>
+                                    Prista Oil-Hungary Kft.
+                                <br></br>
+                                Gorgey utca 4.
+                                <br></br>
+                                H-1224 Budapest
+                                <br></br>
+                                tel: +3620 328 7486
+                                <br></br>
+                                    <a href="mailto:vevo.szolgalat@prista-oil.com" target="_blank" >vevo.szolgalat@prista-oil.com
+                                </a>
+                                    <br></br>
+                                    <a href="http://www.prista-oil.hu/" target="_blank" >www.prista-oil.hu
+                                </a>
+
+                                </p>
+                            </div>
+                            <h4>Ukrayna</h4>
+                            <div class="body">
+                                <p>
+                                    Prista Oil LLC
+                            <br></br>
+                            04070 Kyiv
+                            <br></br>
+                            10A Naberezhno-Khreschatytska
+                            <br></br>
+                            St.
+                            <br></br>
+                            tel: +380 44 5940892
+                            <br></br>
+                            fax: + 380 44 5940893
+                            <br></br>
+                                    <a href="mailto:info@prista-oil.com.ua" target="_blank" >info@prista-oil.com.ua
+                                </a>
+                                    <br></br>
+                                    <a href="http://www.prista-oil.com/" target="_blank" >www.prista-oil.com
+                                </a>
+                                </p>
+                            </div>
+                        </div>
+                        <div class="span">
+                            <h4>Çek Cumhuriyeti</h4>
+                            <div class="body">
+                                <p>
+                                    Prista Oil Czech s.r.o.
+                                <br></br>
+                                19000 Prague
+                                <br></br>
+                                885/2a Špitálská St.
+                                <br></br>
+                                tel: +420 266 799 150
+                                <br></br>
+                                    <a href="mailto:objednavky@prista-oil.cz" target="_blank" >objednavky@prista-oil.cz
+                                </a>
+                                    <br></br>
+                                    <a href="http://www.prista-oil.cz/" target="_blank" >www.prista-oil.cz
+                                </a>
+                                
+                                </p>
+                            </div>
+                            <h4>Türkiye</h4>
+                            <div clas="body">
+                                <p>
+                                Prista Deniz Yağları Tic.Ltd.Şti.
+                                <br></br>
+                                Mahmutbey Mah. Taşoğcağı Yolu Cad.
+                                <br></br>
+                                No:3, Ağaoğlu My Office, Kat:18,
+                                <br></br>
+                                34218 Bağcılar , İstanbul
+                                <br></br>
+                                Tel: +90 212 465 20 05
+                                <br></br>
+                                Fax: +90 212 465 23 11
+                                <br></br>
+                                    <a href="mailto:ozkan.ozturk@prista-oil.com.tr" target="_blank" >ozkan.ozturk@prista-oil.com.tr
+                                </a>
+                                </p>
+                            </div>
+                        </div>
+                        <div class="span">
+                            <h4>Sırbistan</h4>
+                            <div class="body">
+                                <p>
+                                Prista Oil AD Beograd
+                                <br></br>
+                                11070 Belgrade
+                                <br></br>
+                                65 Zorana Djindjica 
+                                <br></br>
+                                Blvd.
+                                <br></br>
+                                tel: +381 11 4075571
+                                <br></br>
+                                fax: +381 11 2606027
+                                <br></br>
+                                    <a href="mailto:office@prista-oil.rs" target="_blank" >office@prista-oil.rs
+                                </a>
+                                    <br></br>
+                                    <a href="http://www.prista-oil.rs/" target="_blank" >www.prista-oil.rs
+                                </a>
+                                </p>
+                            </div>
+                            <h4>Özbekistan</h4>
+                            <div class="body">
+                                <p>
+                                "Uz-Prista" JV LLC
+                                <br></br>
+                                100017 Tashkent
+                                <br></br>
+                                93-A Sharaf Rashidov 
+                                <br></br>
+                                ave.
+                                <br></br>
+                                tel: + 99 871 1206776
+                                <br></br>
+                                fax: +99 871 1206688
+                                <br></br>
+                                    <a href="mailto:info@prista-oil.uz" target="_blank" >info@prista-oil.uz
+                                </a>
+                                    <br></br>
+                                    <a href="http://www.prista-oil.uz/" target="_blank" >www.prista-oil.uz
+                                </a>
+                                </p>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+        </footer>
     )
 }
 
