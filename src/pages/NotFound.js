424d02762a5a3ab82e5242148a7c2d1591ca463a
SevenInterval <ridvanozturk94@gmail.com>
2024-04-08 15:33:58 +0300

    SiteMap, Search ve NotFound Seo çalışmaları tamamlandı

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,8 +1,19 @@
+import { Helmet } from "react-helmet-async";
+
 const NotFound = () => {
     return (
-        <div className="page-404">
-            <strong style={{ fontFamily: "sans-serif" }}>404</strong> We are sorry but this content is not available any longer. Please go to another page.
-        </div>
+        <>
+            <Helmet>
+                <title>Prista Oil</title>
+                <meta name="description"
+                    content="PRISTA Oil Group is a holding company whose field of activity covers the following areas: Production of motor and industrial oils, greases and specialty fluids" />
+                <link rel='canonical' href="*" />
+            </Helmet>
+            <div className="page-404">
+                <h1 style={{ display: "none" }}>Prista Oil &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</h1>
+                <strong style={{ fontFamily: "sans-serif" }}>404</strong> We are sorry but this content is not available any longer. Please go to another page.
+            </div>
+        </>
     )
 }
 
--- src/pages/Search.js
+++ src/pages/Search.js
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import { Link, useLocation } from "react-router-dom";
 import pageItems from "../utility/pageItems";
 import newsItems from "../utility/newsItems";
+import { Helmet } from "react-helmet-async";
 
 const Search = () => {
     const location = useLocation();
@@ -38,41 +39,49 @@ const Search = () => {
     }
 
     return (
-        <div className="container">
-            <article>
-                <h1 class="hs"> Search Result</h1>
-                <div className="search-form">
-                    <form name="fw_search" onSubmit={(event) => handleSearch(event)}>
-                        <div class="fl">
-                            <select id="fw_search_searchKeyName" name="fw_search[searchKeyName]" value={optionValue} onChange={(event) => handleOptionChange(event)}>
-                                <option value="page" >Page</option>
-                                <option value="news" >Haberler</option>
-                                {/*<option value="project">Product</option> */}
-                            </select>
-                            <button type="submit">Submit</button>
-                        </div>
-                        <div className="word">
-                            <input type="text" id="fw_search_query" name="fw_search[query]" value={searchValue}
-                                required="required" placeholder="Search" onChange={(event) => handleChange(event)} />
-                        </div>
-                    </form>
-                </div>
-                {responseData && responseData.length > 0 ?
-                    <ul className="search-list">
-                        {responseData.map(response => {
-                            return (
-                                <li>
-                                    <Link to={response.uri}>{response.label}</Link>
-                                </li>
-                            )
-                        })}
-                    </ul>
-                    :
-                    <h3 class="hs-errors">Not Found</h3>
-                }
+        <>
+            <Helmet>
+                <title>Search Result</title>
+                <meta name="description"
+                    content="PRISTA Oil Group is a holding company whose field of activity covers the following areas: Production of motor and industrial oils, greases and specialty fluids" />
+                <link rel='canonical' href="/site/search" />
+            </Helmet>
+            <div className="container">
+                <article>
+                    <h1 class="hs"> Search Result&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</h1>
+                    <div className="search-form">
+                        <form name="fw_search" onSubmit={(event) => handleSearch(event)}>
+                            <div class="fl">
+                                <select id="fw_search_searchKeyName" name="fw_search[searchKeyName]" value={optionValue} onChange={(event) => handleOptionChange(event)}>
+                                    <option value="page" >Page</option>
+                                    <option value="news" >Haberler</option>
+                                    {/*<option value="project">Product</option> */}
+                                </select>
+                                <button type="submit">Submit</button>
+                            </div>
+                            <div className="word">
+                                <input type="text" id="fw_search_query" name="fw_search[query]" value={searchValue}
+                                    required="required" placeholder="Search" onChange={(event) => handleChange(event)} />
+                            </div>
+                        </form>
+                    </div>
+                    {responseData && responseData.length > 0 ?
+                        <ul className="search-list">
+                            {responseData.map(response => {
+                                return (
+                                    <li>
+                                        <Link to={response.uri}>{response.label}</Link>
+                                    </li>
+                                )
+                            })}
+                        </ul>
+                        :
+                        <h3 class="hs-errors">Not Found</h3>
+                    }
 
-            </article>
-        </div>
+                </article>
+            </div>
+        </>
     )
 }
 
